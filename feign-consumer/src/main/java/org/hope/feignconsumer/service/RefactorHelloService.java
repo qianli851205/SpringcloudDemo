78f5eebcc7ab2d35a1007301c88c3fb96491bda3
qianli851205 <372920173@qq.com>
2018-07-04 17:55:23 +0800

    ss

--- feign-consumer/src/main/java/org/hope/feignconsumer/service/RefactorHelloService.java
+++
@@ -1,8 +0,0 @@
-package org.hope.feignconsumer.service;
-
-import org.hope.service.HelloService;
-import org.springframework.cloud.netflix.feign.FeignClient;
-
-@FeignClient("service-hi")
-public interface RefactorHelloService extends HelloService {
-}
